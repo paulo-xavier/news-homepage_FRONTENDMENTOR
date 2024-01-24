12e46a9890fe82507dbd1c82a9a2b5dab4037dbe
paulo-xavier <px568661@gmail.com>
2024-01-24 01:36:21 -0300

    :lipstick: feat: Implementation of responsivity for all devices, hamburguer menu, refactoring of HTML and scrollbar style.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,20 @@ const hamburguerMenu = document.getElementById('hamburguer-menu');
 const navItems = document.getElementById('nav-items')
 const main = document.querySelector('main'); 
 
+const navItem = document.querySelectorAll('.nav-item'); 
+
 hamburguerMenu.addEventListener('click', () => {
 
     navItems.classList.toggle('active'); 
     hamburguerMenu.classList.toggle('active'); 
     main.classList.toggle('active');
-    
 })
+
+navItem.forEach(item => item.addEventListener('click', () => {
+    const navItemsIsActive = navItems.classList.contains('active'); 
+
+    if (navItemsIsActive) {
+        navItems.classList.remove('active'); 
+        hamburguerMenu.classList.remove('active');
+    }
+})); 
